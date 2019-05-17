1fe2767dfb671a08a6666325ab03c413313b9c75
Smelky <46086711+Smelky@users.noreply.github.com>
2019-05-17 21:31:52 +0300

    Done task

--- src/main/java/net/spring/listener/StudentCreatableListener.java
+++ src/main/java/net/spring/listener/StudentCreatableListener.java
@@ -1,1 +1,14 @@
 
+package net.spring.listener;
+
+import net.spring.entity.CreatableEntity;
+
+import javax.persistence.PrePersist;
+import java.util.Date;
+
+public class StudentCreatableListener {
+    @PrePersist
+    public void prePersist(CreatableEntity entity) {
+        entity.setCreatedDate(new Date().getTime());
+    }
+}
