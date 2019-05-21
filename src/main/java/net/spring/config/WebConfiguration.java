400271f3946505dcb78660ccdefcd592bc8936fe
Smelky <46086711+Smelky@users.noreply.github.com>
2019-05-21 16:41:24 +0300

    Added static modifiers Added annotations Changed types to LocalDate Changed meta charset to UTF-8

--- src/main/java/net/spring/config/WebConfiguration.java
+++ src/main/java/net/spring/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package net.spring.config;
 
+import lombok.RequiredArgsConstructor;
 import net.spring.interceptor.StudentInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class WebConfiguration implements WebMvcConfigurer {
-    @Autowired
-    StudentInterceptor studentInterceptor;
+    private final StudentInterceptor studentInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/net/spring/listener/StudentCreatableListener.java
+++ src/main/java/net/spring/listener/StudentCreatableListener.java
@@ -1,14 +1,13 @@
-
 package net.spring.listener;
 
 import net.spring.entity.CreatableEntity;
 
 import javax.persistence.PrePersist;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class StudentCreatableListener {
     @PrePersist
     public void prePersist(CreatableEntity entity) {
-        entity.setCreatedDate(new Date().getTime());
+        entity.setCreatedDate(LocalDateTime.now());
     }
 }
